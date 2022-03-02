b9049e62598ba2ecbfc4480cbcdc04830d3d78fc
Jia Hong <kuanjiahong@gmail.com>
2022-03-02 23:32:05 +0800

    Removed text-center

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="text-center bg-black text-white mt-100">
+        <footer className="p-2 bg-black text-white mt-100">
             Created by <a href="https://github.com/kuanjiahong" target="_blank" rel="noreferrer">kuanjiahong</a>
         </footer>
     )
